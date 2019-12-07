35436eec6a82f762c99ae2cbffeced328d49acb3
aviadbu <aviad810@gmail.com>
2019-12-07 16:46:38 +0200

    check git in IDE

--- assests.py
+++ assests.py
@@ -1,12 +1,12 @@
 # https://imggmi.com/
 
-# 640pxl
+# 640 pxl
 # MAIN_VERSION = 'https://cdn1.imggmi.com/uploads/2019/12/6/de6bdaec0d3115980e3b67ae19f14b93-full.png'
 # REF_VERSION = 'https://cdn1.imggmi.com/uploads/2019/12/6/ed68397930636052e7a048e2f49a58d8-full.png'
 # similar main to ref
 # MAIN_VERSION = 'https://cdn1.imggmi.com/uploads/2019/12/6/ed68397930636052e7a048e2f49a58d8-full.png'
 
-# 320pxl
+# 320 pxl
 MAIN_VERSION = 'https://cdn1.imggmi.com/uploads/2019/12/7/08c4898116571511a853fbc1d599dbe7-full.png'
 REF_VERSION = 'https://cdn1.imggmi.com/uploads/2019/12/7/6895be7b5327d293773282a5bdf12bfe-full.png'
 # similar main to ref
