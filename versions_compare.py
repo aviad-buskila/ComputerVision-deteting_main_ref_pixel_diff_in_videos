6432b1105f14aeae33d68373fd38fb77e97f9b83
Aviad Buskila <aviadbuskila@Aviads-MacBook-Pro.local>
2019-12-07 16:41:01 +0200

    refactor to mobile device, add default versions, add arguments option

--- versions_compare.py
+++ versions_compare.py
@@ -7,6 +7,7 @@ import datetime
 import assests
 import calculator
 import configurations
+import argparse
 
 
 # document timestamp for image mark and debug abilities
@@ -17,7 +18,10 @@ def prettify_now():
 # Initializing chrome instance, returns driver object
 def initialize_chrome_instance(url):
     options = Options()
+    mobile_emulation = {"deviceName": "iPhone X"}
     # options.add_argument('--headless')
+    # options.add_argument('--disable-gpu')
+    options.add_experimental_option("mobileEmulation", mobile_emulation)
     driver = Chrome('assets/chromedriver', options=options)
     driver.get(url)
     return driver
@@ -25,7 +29,7 @@ def initialize_chrome_instance(url):
 
 # Navigate to proper endpoint, retrieve the element, take its screenshot and store it in products directory.
 # Function returns screenshot file path
-def take_element_screenshot(element_path, version, timestamp):
+def take_element_screenshot(element_path, version, tag, timestamp):
     driver = initialize_chrome_instance(element_path)
     wait = WebDriverWait(driver, configurations.WAIT_TIMEOUT)
     image = wait.until(EC.presence_of_element_located((By.TAG_NAME, configurations.SELECTOR_TAG)))
@@ -44,9 +48,17 @@ def compare_versions(main_version, ref_version):
 
 
 def main():
+    parser = argparse.ArgumentParser(add_help=True)
+    parser.add_argument('-main', action="store", dest="main_version", default=assests.MAIN_VERSION,
+                        help="main web page")
+    parser.add_argument('-ref', action="store", dest="ref_version", default=assests.REF_VERSION,
+                        help="ref web page")
+    parser.add_argument('-tag', action="store", dest="tag", default=configurations.SELECTOR_TAG,
+                        help="selector attribute")
+    arguments = parser.parse_args()
     time_stamp = prettify_now()
-    main_image = take_element_screenshot(assests.MAIN_VERSION, "main", time_stamp)
-    ref_image = take_element_screenshot(assests.REF_VERSION, "ref", time_stamp)
+    main_image = take_element_screenshot(arguments.main_version, "main", arguments.tag, time_stamp)
+    ref_image = take_element_screenshot(arguments.ref_version, "ref", arguments.tag, time_stamp)
     main_values = calculator.calculate_signal_vs_background(configurations.BACKGROUND, main_image)
     ref_values = calculator.calculate_signal_vs_background(configurations.BACKGROUND, ref_image)
     if compare_versions(main_values, ref_values):
